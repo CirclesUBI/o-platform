d9a8a3f1a7ea0fbc59f5ee1f70964a959c845af8
Ingo Collatz <incollatz@mail.de>
2022-11-03 16:17:18 +0100

    added favorite-icon to BusinessListCard

--- shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
+++ shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
@@ -6,26 +6,33 @@ import SimpleHeader from "../../../shared/atoms/SimpleHeader.svelte";
 import { AllBusinessesDocument, Businesses } from "../../../shared/api/data/types";
 import { onMount } from "svelte";
 import { ApiClient } from "../../../shared/apiConnection";
+import BusinessCard from "../atoms/BusinessCard.svelte";
 
 export let runtimeDapp: RuntimeDapp<any>;
 export let routable: Routable;
 
-let businesses: [Businesses];
+let businesses: Businesses[] = [];
 
 onMount(async () => {
-  getBusinesses()
+  getBusinesses();
 });
 
 async function getBusinesses() {
-  let queryResult = await ApiClient.query<[Businesses], null>(AllBusinessesDocument, null)
+  let queryResult = await ApiClient.query<[Businesses], null>(AllBusinessesDocument, null);
 
   businesses = queryResult;
-  console.log(businesses)
+  console.log(businesses);
 }
 </script>
 
 <SimpleHeader runtimeDapp="{runtimeDapp}" routable="{routable}" />
 
-<section class="">
-
+<section class="p-4 mx-auto mb-20 -mt-3 md:w-2/3 xl:w-1/2 flex flex-wrap justify-evenly content-center"> 
+  {#each businesses as business}
+    <BusinessCard
+      name="{business.name}"
+      id="{business.id}"
+      description="{business.description}"
+      image="{business.picture}" />
+  {/each}
 </section>
