c0d13289b08efb09ea4324d5cf3af5569c9e38dc
Dacian Avram <dacian.avram@justrocket.ro>
2022-11-08 13:39:36 +0200

    add survey homepage

---
+++ shell/src/dapps/o-homepage/pages/Survey.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import "../../../shared/css/tailwind.css";
+import TopNav from "../../../shared/atoms/TopNav.svelte";
+import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
+import { Routable } from "@o-platform/o-interfaces/dist/routable";
+import VideoHeader from "../components/VideoHeader.svelte";
+
+export let runtimeDapp: RuntimeDapp<any>;
+export let routable: Routable;
+export let inviteCode: any;
+
+if (inviteCode) {
+  localStorage.setItem("circlesInvite", inviteCode);
+  window.runInitMachine();
+}
+</script>
+
+<div class="flex flex-col overflow-hidden ">
+  <h1>Survey</h1>
+  <main class="h-screen overflow-hidden">
+    <TopNav runtimeDapp="{runtimeDapp}" routable="{routable}" />
+
+    <VideoHeader />
+  </main>
+</div>
