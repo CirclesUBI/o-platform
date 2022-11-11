19cff1efa7a4953e0b1d3b707ef80d141974a985
Dacian Avram <dacian.avram@justrocket.ro>
2022-11-11 18:16:28 +0200

    create suvey components and routes

--- shell/src/dapps/o-homepage/components/InformedConsentFirstSurvey.svelte
+++ shell/src/dapps/o-homepage/components/InformedConsentFirstSurvey.svelte
@@ -1,120 +1,13 @@
-<script>
-import Label from "../../../shared/atoms/Label.svelte";
-import { _ } from "svelte-i18n";
-import { push } from "svelte-spa-router";
-
-let firstCheckbox = false;
-let secondCheckbox = false;
-let thirdCheckbox = false;
-let isValid = false;
-
-function handleToggleCheckbox(event, message) {
-  if (event.target.checked) {
-    if (message === "first") {
-      firstCheckbox = true;
-    }
-    if (message === "second") {
-      secondCheckbox = true;
-    }
-    if (message === "third") {
-      thirdCheckbox = true;
-    }
-  } else {
-    if (message === "first") {
-      firstCheckbox = false;
-    }
-    if (message === "second") {
-      secondCheckbox = false;
-    }
-    if (message === "third") {
-      thirdCheckbox = false;
-    }
-  }
-}
-
-$: {
-  isValid = firstCheckbox && secondCheckbox && thirdCheckbox;
-}
-
-function handleClick(button) {
-  if (button === "back") {
-    push("#/homepage/survey/1");
-  } else {
-    if (isValid) {
-      push("#/homepage/survey/3");
-    }
-  }
-}
-</script>
-
-<div class="overflow-hidden text-white whitespace-pre-line bg-clip-content">
-  <div class="px-5 py-5 text-center uppercase bg-negative">
-    <div class="survey-text">
-      <Label key="dapps.o-homepage.components.survey.informedConsent.title.top" />
-    </div>
-    <div class="text-primary">
-      <Label key="dapps.o-homepage.components.survey.informedConsent.title.middle" />
-    </div>
-    <div class="whitespace-pre-line">
-      <Label key="dapps.o-homepage.components.survey.informedConsent.title.bottom" />
-    </div>
+<div>
+  <div>
+    Before you can use this app you need to agree to the following points. If you agree, press the boxes to tick. If you
+    have any questions, please reach out to a local circles representative.
   </div>
-  <div class="mx-auto mb-20 md:w-2/3 xl:w-1/2">
-    <div class="mx-10 my-5 uppercase">
-      <input
-        id="check-1"
-        type="checkbox"
-        class="mr-2 checkbox checkbox-warning"
-        on:change="{(event) => handleToggleCheckbox(event, 'first')}" />
-      <label for="check-1" class="cursor-pointer">
-        <Label key="dapps.o-homepage.components.survey.informedConsent.firstCheckbox" />
-      </label>
-    </div>
-    <div class="mx-10 my-5 uppercase">
-      <input
-        id="check-2"
-        type="checkbox"
-        class="mr-2 checkbox checkbox-warning"
-        on:change="{(event) => handleToggleCheckbox(event, 'second')}" />
-      <label for="check-2" class="cursor-pointer">
-        <Label key="dapps.o-homepage.components.survey.informedConsent.secondCheckbox" />
-      </label>
-    </div>
-    <div class="mx-10 my-5 uppercase">
-      <input
-        id="check-3"
-        type="checkbox"
-        class="mr-2 checkbox checkbox-warning"
-        on:change="{(event) => handleToggleCheckbox(event, 'third')}" />
-      <label for="check-3" class="cursor-pointer">
-        <Label key="dapps.o-homepage.components.survey.informedConsent.thirdCheckbox" />
-      </label>
-    </div>
-    {#if !isValid}
-      <div class="mx-10 my-5 text-sm text-info text-center">
-        <Label key="dapps.o-homepage.components.survey.informedConsent.info" />
-      </div>
-    {/if}
-    <div class="buttons-container flex flex-row justify-around mt-10 mb-5 text-center">
-      <div>
-        <button
-          class="relative px-8 overflow-hidden transition-all transform btn bg-cpurple border-warning text-warning"
-          on:click="{() => handleClick('back')}">
-          {$_("dapps.o-homepage.components.survey.button.goBack")}</button>
-      </div>
-      <div>
-        <button
-          class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple"
-          on:click="{() => handleClick('next')}"
-          disabled="{!isValid}">
-          {$_("dapps.o-homepage.components.survey.button.next")}</button>
-      </div>
-    </div>
+  <div>I understand that Circles is conduction a research project.</div>
+  <div>I agree to participate in this Circles research project.</div>
+  <div>
+    I understand that when the Circles research project end I may not be able to use Circles points that remain in my
+    Circles wallet.
   </div>
+  <div>You haven't ticked all the boxes so you cannot use the Circles app.</div>
 </div>
-
-<style>
-  :global(.buttons-container) {
-    margin-top: 200px;
-  }
-  </style>
--- shell/src/dapps/o-homepage/components/WelcomeSurvey.svelte
+++ shell/src/dapps/o-homepage/components/WelcomeSurvey.svelte
@@ -1,51 +1,15 @@
 <script lang="ts">
-import Button from "../../../shared/atoms/button/Button.svelte";
+import { dapps } from "../../../loader";
 import Label from "../../../shared/atoms/Label.svelte";
-import { _ } from "svelte-i18n";
-import { push } from "svelte-spa-router";
-
-function handleClick() {
-  push("#/homepage/survey/2");
-}
 </script>
 
-<div class="p-1 pr-4 -mt-6 overflow-hidden text-white whitespace-pre-line  xs:p-3 xs:-mt-2 font-heading">
-  <div class="flex flex-col items-center justify-center">
-    <div class="ml-2 text-white uppercase text-4xl">
-      <Label key="dapps.o-homepage.components.survey.title.top" />
-    </div>
-    <div class="flex items-center justify-center my-5">
-      <img src="/logos/circles.svg" class="w-32 h-32" alt="Circles Land" />
-    </div>
-
-    <div class="ml-2 text-white uppercase text-4xl font-heading">
-      <Label key="dapps.o-homepage.components.survey.title.bottom" />
-    </div>
+<div>
+  <div class="p-1 xs:p-3 pr-4 xs:pr-12 -mt-6 xs:-mt-2 whitespace-nowrap">
+    <Label key="dapps.o-homepage.components.survey.title.top" />
+    <img src="/logos/circles.svg" class="w-6 h-6 xs:w-32 xs:h-32" alt="Circles Land" />
+    <Label key="dapps.o-homepage.components.survey.title.bottom" />
   </div>
-  <div class="mt-2 text-center text-xl"><Label key="dapps.o-homepage.components.survey.subtitle" /></div>
-  <div class="mb-8 text-center uppercase mt-14 text-xl"><Label key="dapps.o-homepage.components.survey.circleIs" /></div>
-  <div class="text-center uppercase whitespace-pre-line text-xl">
-    <Label key="dapps.o-homepage.components.survey.eachCircle" />
-  </div>
-  <div class="buttons-container flex flex-row justify-around mt-10 mb-5 text-center">
-    <div>
-      <button
-        class="relative px-8 overflow-hidden transition-all transform btn btn-light bg-cpurple border-warning text-warning svelte-12kbnbk"
-        on:click="{() => handleClick()}">
-        {$_("dapps.o-homepage.components.survey.button.goBack")}</button>
-    </div>
-    <div>
-      <button
-        class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple"
-        on:click="{() => handleClick()}">
-        {$_("dapps.o-homepage.components.survey.button.next")}</button>
-    </div>
-  </div>
+  <div><Label key="dapps.o-homepage.components.survey.subtitle" /></div>
+  <div><Label key="dapps.o-homepage.components.survey.circleIs" /></div>
+  <div><Label key="dapps.o-homepage.components.survey.eachCircle" /></div>
 </div>
-
-
-<style>
-  :global(.buttons-container) {
-    margin-top: 180px;
-  }
-  </style>
