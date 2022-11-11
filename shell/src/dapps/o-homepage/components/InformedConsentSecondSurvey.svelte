80f6c28093a38e5aa1dc511edffe07c85301bf2c
Dacian Avram <dacian.avram@justrocket.ro>
2022-11-11 18:16:28 +0200

    create suvey components and routes

---
+++ shell/src/dapps/o-homepage/components/InformedConsentSecondSurvey.svelte
@@ -0,0 +1,12 @@
+<div>
+  <div>
+    Before you can use this app you need to agree to the following points. If you agree, press the boxes to tick. If you
+    have any questions, please reach out to a local circles representative.
+  </div>
+  <div>I understand that Circles points cannot be exchanged for Rupiah or any other currency.</div>
+  <div>
+    I understand that Circles project will be collecting information I provide in this app as a means of better
+    understanding how Circles can work in my area.
+  </div>
+  <div>You haven't ticked all the boxes so you cannot use the Circles app.</div>
+</div>
