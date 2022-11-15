1be01ca3fb30078ddffa7165634baff85fc60124
jaensen <daniel@circles.name>
2022-11-15 14:27:39 +0100

    wire up the 'Sort by closest' button and use the browser's geolocation api to find the user's location.

--- shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
+++ shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
@@ -5,8 +5,8 @@
   import SimpleHeader from "../../../shared/atoms/SimpleHeader.svelte";
   import {
     AllBusinessesDocument,
-    Businesses,
-    MutationToggleFavoriteArgs,
+    Businesses, Maybe,
+    MutationToggleFavoriteArgs, Profile,
     ToggleFavoriteDocument
   } from "../../../shared/api/data/types";
   import {onMount} from "svelte";
@@ -14,28 +14,37 @@
   import BusinessCard from "../atoms/BusinessCard.svelte";
   import Icon from "@krowten/svelte-heroicons/Icon.svelte";
   import {me} from "../../../shared/stores/me";
+  import {myLocation} from "src/shared/stores/myLocation";
 
   export let runtimeDapp: RuntimeDapp<any>;
   export let routable: Routable;
 
   let businesses: Businesses[] = [];
   let favorites: {[circlesAddress:string]: boolean} = {};
+  let currentLocation: GeolocationPosition|GeolocationPositionError|Error|null;
 
 onMount(async () => {
-  await Promise.all([
+  const results = await Promise.all([
           getBusinesses(),
           me.reload()
   ]);
-  me.subscribe(m => {
-    favorites = {};
-    m.favorites.forEach(f => {
+  businesses = <any>results[0];
+  const profile:Maybe<Profile> = <any>results[1];
+
+  if (profile) {
+    profile.favorites.forEach(f => {
       favorites[f.favorite.circlesAddress] = true;
     });
-  })
+  }
 });
 
-async function getBusinesses() {
-  businesses = await ApiClient.query<[Businesses], null>(AllBusinessesDocument, null);
+async function getBusinesses() : Promise<Businesses[]> {
+  if (currentLocation) {
+    console.log("Sorting result by 'closest first' for location: ", currentLocation);
+    return await ApiClient.query<Businesses[], null>(AllBusinessesDocument, null);
+  } else {
+    return await ApiClient.query<Businesses[], null>(AllBusinessesDocument, null);
+  }
 }
 
 async function toggleFavorite(circlesAddress:string) {
@@ -52,17 +61,31 @@ async function toggleFavorite(circlesAddress:string) {
   <div class="flex self-center justify-center content-between">
     <button class="btn mr-2 ml-2 text-black bg-white border-1"
       ><span><Icon name="adjustments" class="h-6 w-6" /></span>Filter by type</button>
-    <button class="btn mr-2 ml-2 text-black bg-white border-1"
+    <button class="btn mr-2 ml-2 text-black bg-white border-1" on:click={() => {
+      myLocation.reload();
+    }}
       ><span><Icon name="chevron-down" class="h-6 w-6" /></span>Sort by closest</button>
   </div>
 
   <div class="p-4 mx-auto mb-20 -mt-3 md:w-2/3 xl:w-1/2 flex flex-wrap justify-evenly content-center">
-    {#each businesses as business}
-      <BusinessCard
-          on:toggleFavorite={e => toggleFavorite(e.detail)}
-          business={business}
-          isFavorite={favorites[business.circlesAddress]}
-      />
-    {/each}
+    {#if $myLocation}
+      <!-- Sort nearest first -->
+      <p>Nearest first:</p>
+      {#each businesses as business}
+        <BusinessCard
+                on:toggleFavorite={e => toggleFavorite(e.detail)}
+                business={business}
+                isFavorite={favorites[business.circlesAddress]}
+        />
+      {/each}
+    {:else}
+      {#each businesses as business}
+        <BusinessCard
+                on:toggleFavorite={e => toggleFavorite(e.detail)}
+                business={business}
+                isFavorite={favorites[business.circlesAddress]}
+        />
+      {/each}
+    {/if}
   </div>
 </section>
