da4b1309a901f2fbb83462ea25825b54b914fa67
jaensen <daniel@circles.name>
2022-11-17 01:11:21 +0100

    store the 'location' in createOrganisation

--- shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
+++ shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
@@ -3,65 +3,93 @@
   import {Routable} from "@o-platform/o-interfaces/dist/routable";
 
   import SimpleHeader from "../../../shared/atoms/SimpleHeader.svelte";
-  import {
-    AllBusinessesDocument,
-    Businesses,
-    MutationToggleFavoriteArgs,
-    ToggleFavoriteDocument
-  } from "../../../shared/api/data/types";
-  import {onMount} from "svelte";
-  import {ApiClient} from "../../../shared/apiConnection";
   import BusinessCard from "../atoms/BusinessCard.svelte";
   import Icon from "@krowten/svelte-heroicons/Icon.svelte";
-  import {me} from "../../../shared/stores/me";
+  import {myLocation} from "src/shared/stores/myLocation";
+  import {businesses} from "src/shared/stores/businesses";
+  import {QueryAllBusinessesOrderOptions} from "src/shared/api/data/types";
+  import {onMount} from "svelte";
+  import {BusinessCategory} from "../../../shared/api/data/types";
+  import {ApiClient} from "../../../shared/apiConnection";
 
   export let runtimeDapp: RuntimeDapp<any>;
   export let routable: Routable;
 
-  let businesses: Businesses[] = [];
-  let favorites: {[circlesAddress:string]: boolean} = {};
+  let categories: BusinessCategory[] = [];
 
-onMount(async () => {
-  await Promise.all([
-          getBusinesses(),
-          me.reload()
-  ]);
-  me.subscribe(m => {
-    favorites = {};
-    m.favorites.forEach(f => {
-      favorites[f.favorite.circlesAddress] = true;
-    });
-  })
-});
-
-async function getBusinesses() {
-  businesses = await ApiClient.query<[Businesses], null>(AllBusinessesDocument, null);
-}
-
-async function toggleFavorite(circlesAddress:string) {
-  favorites[circlesAddress] = await ApiClient.mutate<boolean, MutationToggleFavoriteArgs>(ToggleFavoriteDocument, {
-    circlesAddress: circlesAddress
+  onMount(async () => {
+    categories = await ApiClient.query<BusinessCategory[], any>();
   });
-  return favorites[circlesAddress];
-}
 </script>
 
+<div style="visibility: hidden;" class="bg-market"></div>
 <SimpleHeader runtimeDapp="{runtimeDapp}" routable="{routable}" />
 <section class="justify-center align-middle">
-  <div class="flex self-center justify-center content-between">
-    <button class="btn mr-2 ml-2 text-black bg-white border-1"
-      ><span><Icon name="adjustments" class="h-6 w-6" /></span>Filter by type</button>
-    <button class="btn mr-2 ml-2 text-black bg-white border-1"
-      ><span><Icon name="chevron-down" class="h-6 w-6" /></span>Sort by closest</button>
+  <div class="p-4 pt-0 mx-auto -mt-6 md:w-2/3 xl:w-1/2 flex justify-around">
+    <div class="w-36 dropdown">
+      <button class="btn w-36 text-black bg-white border-1"
+      ><span><Icon name="adjustments" class="h-6 w-6" /></span>Filter</button>
+      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+
+        <li on:click={() => {
+        businesses.reload(QueryAllBusinessesOrderOptions.MostPopular);
+      }}><a>Sort by most popular</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Nearest);
+        }}>Sort by nearest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Newest);
+        }}>Sort by newest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Oldest);
+        }}>Sort by oldest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Alphabetical);
+        }}>Sort by name</a></li>
+      </ul>
+    </div>
+    <!--
+      myLocation.reload(); -->
+      <div class="w-36 dropdown dropdown-end">
+      <button class="btn w-36 text-black bg-white border-1"
+      ><span><Icon name="adjustments" class="h-6 w-6" /></span>Sort</button>
+      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+        <li on:click={() => {
+        businesses.reload(QueryAllBusinessesOrderOptions.MostPopular);
+      }}><a>Sort by most popular</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Nearest);
+        }}>Sort by nearest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Newest);
+        }}>Sort by newest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Oldest);
+        }}>Sort by oldest</a></li>
+        <li><a on:click={() => {
+          businesses.reload(QueryAllBusinessesOrderOptions.Alphabetical);
+        }}>Sort by name</a></li>
+      </ul>
+    </div>
   </div>
 
   <div class="p-4 mx-auto mb-20 -mt-3 md:w-2/3 xl:w-1/2 flex flex-wrap justify-evenly content-center">
-    {#each businesses as business}
-      <BusinessCard
-          on:toggleFavorite={e => toggleFavorite(e.detail)}
-          business={business}
-          isFavorite={favorites[business.circlesAddress]}
-      />
-    {/each}
+    {#if $myLocation}
+      <!-- Sort nearest first -->
+      <p>Nearest first:</p>
+      {#each $businesses.sort((a,b) => a.index > b.index ? 1: a.index < b.index ? -1 : 0) as business}
+        <BusinessCard
+                on:toggleFavorite={e => businesses.toggleFavorite(e.detail)}
+                business={business}
+        />
+      {/each}
+    {:else}
+      {#each $businesses.sort((a,b) => a.index > b.index ? 1: a.index < b.index ? -1 : 0) as business}
+        <BusinessCard
+                on:toggleFavorite={e => businesses.toggleFavorite(e.detail)}
+                business={business}
+        />
+      {/each}
+    {/if}
   </div>
 </section>
