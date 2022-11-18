c127e44916bbe51124daaa67ee10bbcbc85d7c40
Dacian Avram <dacian.avram@justrocket.ro>
2022-11-18 19:07:36 +0200

    add Dropdown component

---
+++ shell/src/shared/molecules/DropDown.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    
+    export let items: any;
+    export let key: string;
+    export let value: string; 
+    export let id: string;
+    export let selected: string;
+    
+    const dispatch = createEventDispatcher();
+    
+    function handleOnChange(e) {
+        console.log(e.target.value)
+        dispatch('dropDownChange', {target: id ,value: e.target.value});
+    }
+    
+    </script>
+    
+    
+    <select class="select w-full max-w-xs text-black mt-2" on:change={handleOnChange}>
+        <option value={undefined} selected>{selected}</option>
+        {#each items as item}
+            <option value={item[key]}>{item[value]}</option>
+        {/each}
+    
+      </select>
