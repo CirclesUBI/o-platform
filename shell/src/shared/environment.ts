7911b9eaaf73938783fdc6e044274cfde0a7625c
jaensen <daniel@circles.name>
2022-11-18 18:53:31 +0100

    get the build config entirely from environment variables + clean up unused config values.

--- shell/src/shared/environment.ts
+++ shell/src/shared/environment.ts
@@ -7,48 +7,8 @@ export class Environment {
     return "__SHOW_LANGUAGE_SWITCHER__" == "true";
   }
 
-  public static get authEndpointUrl(): string {
-    return "__AUTH_ENDPOINT__";
-  }
-
   public static get apiEndpointUrl(): string {
     return "__API_ENDPOINT__";
-  }
-
-  public static get humanodeAuthUrl(): string {
-    return "__HUMANODE_AUTH_URL__";
-  }
-
-  public static get humanodeTokenUrl(): string {
-    return "__HUMANODE_TOKEN_URL__";
-  }
-
-  public static get humanodeRedirectUrl(): string {
-    return "__HUMANODE_REDIRECT_URL__";
-  }
-
-  public static get humanodeClientId(): string {
-    return "__HUMANODE_CLIENT_ID__";
-  }
-
-  public static get humanodeScope(): string {
-    return "__HUMANODE_SCOPE__";
-  }
-
-  public static get appId(): string {
-    return "__APP_ID__";
-  }
-
-  public static get externalUrl(): string {
-    return "__EXTERNAL_URL__";
-  }
-
-  public static get filesAppId(): string {
-    return "__FILES_APP_ID__";
-  }
-
-  public static get circlesGardenApiUrl(): string {
-    return "__CIRCLES_GARDEN_API__";
   }
 
   public static get pathfinderEndpointUrl(): string {
@@ -61,10 +21,6 @@ export class Environment {
 
   public static get circlesHubAddress(): string {
     return "__CIRCLES_HUB_ADDRESS__";
-  }
-
-  public static get circlesHubBlock(): number {
-    return parseInt("__CIRCLES_HUB_BLOCK__");
   }
 
   public static get safeProxyFactoryAddress(): string {
@@ -84,20 +40,6 @@ export class Environment {
     return "__USE_MOCKS__" == "true";
   }
 
-  public static get allowVerify(): boolean {
-    // @ts-ignore
-    return "__ALLOW_VERIFY__" == "true";
-  }
-
-  public static get allowCreateOrganisation(): boolean {
-    // @ts-ignore
-    return "__ALLOW_CREATE_ORGANISATION__" == "true";
-  }
-
-  public static get fixedGasPrice(): string {
-    return "__FIXED_GAS_PRICE__";
-  }
-
   public static get openLoginClientId(): string {
     return "__OPENLOGIN_CLIENT_ID__";
   }
@@ -106,12 +48,6 @@ export class Environment {
     return "__HERE_API_KEY__";
   }
 
-  public static getShopMetadata(shopId: number): string {
-    return localStorage.getItem("shopMetadata." + shopId);
-  }
-  public static setShopMetadata(shopId: number, data: string) {
-    return localStorage.setItem("shopMetadata." + shopId, data);
-  }
   public static get userLanguage(): string {
     if (!this.showLanguageSwitcher)
       return "en";
