87cbfe5cd9381291d2a7f1e7e55b98052c995d70
Thorsten Schau <letsgo@codeho.net>
2022-11-21 10:18:55 +0100

    adding lat & long to the map display

--- shell/src/dapps/o-marketlisting/atoms/Map.svelte
+++ shell/src/dapps/o-marketlisting/atoms/Map.svelte
@@ -2,32 +2,21 @@
 import Map from "here-maps-svelte";
 import { onMount } from "svelte";
 import { Environment } from "src/shared/environment";
+
 export let width = "100%";
 export let height = "400px";
-export let hereId = "here:cm:namedplace:20177269"; // TODO: REMOVE HARDCODED
+export let position = {};
 
-let position = {};
 let options;
 
-// Grab Location Info from hereId
-onMount(async () => {
-  fetch(`https://lookup.search.hereapi.com/v1/lookup?id=${hereId}&apiKey=${Environment.hereApiKey}`)
-    .then((response) => response.json())
-    .then((data) => {
-      position = data.position;
-      options = {
-        api: Environment.hereApiKey,
-        mapdata: {
-          zoom: 10,
-          center: position,
-        },
-      };
-    })
-    .catch((error) => {
-      // TODO: HANDLE ERROR CASE
-      console.log(error);
-      return [];
-    });
+onMount(() => {
+  options = {
+    api: Environment.hereApiKey,
+    mapdata: {
+      zoom: 10,
+      center: { lat: position.lat, lng: position.long },
+    },
+  };
 });
 </script>
 
