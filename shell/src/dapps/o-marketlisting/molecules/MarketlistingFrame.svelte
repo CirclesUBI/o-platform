3fcec0a6fb28c64147d50c3cd5b997b04c5c73d2
jaensen <daniel@circles.name>
2022-11-23 06:41:36 +0100

    allow category filtering in the marketlisting

--- shell/src/dapps/o-marketlisting/molecules/MarketlistingFrame.svelte
+++ shell/src/dapps/o-marketlisting/molecules/MarketlistingFrame.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
-  import { Routable } from "@o-platform/o-interfaces/dist/routable";
+  import {RuntimeDapp} from "@o-platform/o-interfaces/dist/runtimeDapp";
+  import {Routable} from "@o-platform/o-interfaces/dist/routable";
 
   import SimpleHeader from "../../../shared/atoms/SimpleHeader.svelte";
   import Icon from "@krowten/svelte-heroicons/Icon.svelte";
-  import { QueryAllBusinessesOrderOptions } from "src/shared/api/data/types";
-  import { onMount } from "svelte";
+  import {QueryAllBusinessesOrderOptions} from "src/shared/api/data/types";
+  import {onMount} from "svelte";
   import {
     AllBusinessCategoriesDocument,
     AllBusinessCategoriesQueryVariables,
     BusinessCategory,
   } from "../../../shared/api/data/types";
-  import { ApiClient } from "../../../shared/apiConnection";
+  import {ApiClient} from "../../../shared/apiConnection";
   import {marketStore} from "../stores/marketStore";
 
   export let runtimeDapp: RuntimeDapp<any>;
@@ -21,6 +21,7 @@
 
   let categories: BusinessCategory[] = [];
   let sortedBy: SortedByTypes = "Most popular";
+  let categoryFilter: number[] = [];
 
   onMount(async () => {
     categories = await ApiClient.query<BusinessCategory[], AllBusinessCategoriesQueryVariables>(
@@ -31,55 +32,79 @@
 </script>
 
 <div style="visibility: hidden;" class="bg-market"></div>
-<SimpleHeader runtimeDapp="{runtimeDapp}" routable="{routable}" />
+<SimpleHeader runtimeDapp="{runtimeDapp}" routable="{routable}"/>
 <section class="justify-center align-middle">
     <div class="flex justify-around p-4 pt-0 mx-auto -mt-6 md:w-2/3 xl:w-1/2">
         <div class="w-36 dropdown">
             <button class="text-black bg-white btn w-36 border-1"
-            ><span><Icon name="adjustments" class="w-6 h-6" /></span>Filter</button>
+            ><span><Icon name="adjustments" class="w-6 h-6"/></span>Filter
+            </button>
             <ul class="p-2 shadow dropdown-content menu bg-base-100 rounded-box w-52">
+                {#if categoryFilter.length > 0}
+                <li>
+                    <button class="block w-full" on:click="{() => {
+                      categoryFilter = [];
+                      marketStore.reload($marketStore.orderBy, categoryFilter);
+                    }}"> x Clear all filters</button>
+                </li>
+                {/if}
                 {#each categories as category}
                     <li>
+                    {#if categoryFilter.indexOf(category.id) >= 0}
                         <button class="block w-full" on:click="{() => {
-              marketStore.reload();
-            }}">{category.name}</button>
+                          categoryFilter.splice(categoryFilter.indexOf(category.id), 1);
+                          categoryFilter = categoryFilter;
+                          marketStore.reload($marketStore.orderBy, categoryFilter);
+                        }}"> ✓ {category.name}</button>
+                    {:else}
+                        <button class="block w-full" on:click="{() => {
+                          categoryFilter = categoryFilter.concat([category.id]);
+                          categoryFilter = categoryFilter;
+                          marketStore.reload($marketStore.orderBy, categoryFilter);
+                        }}">{category.name}</button>
+                    {/if}
                     </li>
                 {/each}
             </ul>
         </div>
         <div class="w-36 dropdown dropdown-end">
             <button class="text-black bg-white btn w-36 border-1 whitespace-nowrap"
-            ><span><Icon name="chevron-down" class="w-6 h-6" /></span>{$marketStore.orderBy}</button>
+            ><span><Icon name="chevron-down" class="w-6 h-6"/></span>{$marketStore.orderBy}</button>
             <ul class="p-2 shadow dropdown-content menu bg-base-100 rounded-box w-52">
                 <li>
                     <button class="block w-full" on:click="{() => {
-            sortedBy = 'Most popular';
-            marketStore.reload(QueryAllBusinessesOrderOptions.MostPopular);
-          }}">Sort by most popular</button>
+                        sortedBy = 'Most popular';
+                        marketStore.reload(QueryAllBusinessesOrderOptions.MostPopular, categoryFilter);
+          }}">Sort by most popular
+                    </button>
                 </li>
                 <li>
                     <button class="block w-full" on:click="{() => {
               sortedBy = 'Nearest';
-              marketStore.reload(QueryAllBusinessesOrderOptions.Nearest);
-            }}">Sort by nearest</button>
+              marketStore.reload(QueryAllBusinessesOrderOptions.Nearest, categoryFilter);
+            }}">Sort by nearest
+                    </button>
                 </li>
                 <li>
                     <button class="block w-full" on:click="{() => {
               sortedBy = 'Newest';
-              marketStore.reload(QueryAllBusinessesOrderOptions.Newest);
-            }}">Sort by newest</button>
+              marketStore.reload(QueryAllBusinessesOrderOptions.Newest, categoryFilter);
+            }}">Sort by newest
+                    </button>
                 </li>
                 <li>
                     <button class="block w-full" on:click="{() => {
               sortedBy = 'Oldest';
-              marketStore.reload(QueryAllBusinessesOrderOptions.Oldest);
-            }}">Sort by oldest</button>
+              marketStore.reload(QueryAllBusinessesOrderOptions.Oldest, categoryFilter);
+            }}">Sort by oldest
+                    </button>
                 </li>
                 <li>
                     <button class="block w-full" on:click="{() => {
               sortedBy = 'Alphabetical';
-              marketStore.reload(QueryAllBusinessesOrderOptions.Alphabetical);
-            }}">Sort by name</button>
+              marketStore.reload(QueryAllBusinessesOrderOptions.Alphabetical, categoryFilter);
+            }}">Sort by name
+                    </button>
                 </li>
             </ul>
         </div>
