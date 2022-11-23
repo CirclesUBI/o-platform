853ebde1ceaefe444a6b583924727959998b877c
jaensen <daniel@circles.name>
2022-11-23 07:22:04 +0100

    put the listing filter values in own store so that it can be used on multiple pages

--- shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
+++ shell/src/dapps/o-marketlisting/pages/Marketlisting.svelte
@@ -6,9 +6,12 @@ import BusinessCard from "../atoms/BusinessCard.svelte";
 import {marketFavoritesStore} from "../stores/marketFavoritesStore";
 import {marketStore} from "../stores/marketStore";
 import MarketlistingFrame from "../molecules/MarketlistingFrame.svelte";
+import {marketFilterStore} from "../stores/marketFilterStore";
 
 export let runtimeDapp: RuntimeDapp<any>;
 export let routable: Routable;
+
+console.log($marketFilterStore);
 </script>
 
 <div style="visibility: hidden;" class="bg-market"></div>
