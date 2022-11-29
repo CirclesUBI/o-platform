041a92eefb99fbf147ddb42a9e0668393df06e17
Thorsten Schau <letsgo@codeho.net>
2022-11-29 10:34:11 +0100

    survey adjustments

--- shell/src/dapps/o-homepage/components/UserDataCollectionSurvey.svelte
+++ shell/src/dapps/o-homepage/components/UserDataCollectionSurvey.svelte
@@ -60,114 +60,117 @@ $: {
 }
 </script>
 
-<div
-  class="flex flex-col items-center justify-center p-1 -mt-6 overflow-hidden text-white whitespace-pre-line bg-cpurple xs:p-3 xs:-mt-2">
-  <div class="flex flex-col items-center justify-center">
-    <div class="ml-2 text-2xl text-white uppercase xs:text-4xl font-heading">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.title.top" />
-    </div>
-    <div class="ml-2 text-2xl text-white uppercase xs:text-4xl font-heading">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.title.bottom" />
-    </div>
-  </div>
-  <div class="mt-5 mb-10"><Label key="dapps.o-homepage.components.survey.userDataCollection.subtitle" /></div>
-  <div>
-    <div class="flex flex-col mb-5 text-sm">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.useCircleAs" />
-      <div class="flex">
-        <DropDown
-          selected="Select your type of user"
-          items="{typeOfUserData}"
-          id="userType"
-          key="id"
-          value="name"
-          on:dropDownChange="{handleOnChange}" />
-        {#if typeOfUser}
-          <span class="text-6xl font-enso"
-            ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
-        {/if}
-        {#if !typeOfUser}
-          <span class="text-6xl font-enso"
-            ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
-        {/if}
+<div class="mx-auto mb-20 md:w-2/3 xl:w-1/2">
+  <div class="flex flex-col items-center justify-center px-4 overflow-hidden text-white whitespace-pre-line">
+    <div class="flex flex-col items-center justify-center">
+      <div class="text-2xl text-white uppercase xs:text-4xl font-heading">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.title.top" />
+      </div>
+      <div class="text-2xl text-center text-white uppercase xs:text-4xl font-heading">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.title.bottom" />
       </div>
     </div>
-    <div class="flex flex-col mb-5 text-sm">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.gender" />
-      <div class="flex">
-        <DropDown
-          selected="Select gender"
-          items="{genderOfUserData}"
-          id="gender"
-          key="id"
-          value="name"
-          on:dropDownChange="{handleOnChange}" />
-        {#if genderOfUser}
-          <span class="text-6xl font-enso"
-            ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
-        {/if}
-        {#if !genderOfUser}
-          <span class="text-6xl font-enso"
-            ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
-        {/if}
-      </div>
-    </div>
-    <div class="flex flex-col items-start mb-2 text-sm">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.bornOn" />
-      <div class="flex items-center h-12 mb-2 date-picker">
-        <DateInput
-          bind:value="{dateOfBirthday}"
-          format="MM-dd-yyyy"
-          placeholder="Choose a date"
-          closeOnSelection="{true}"
-          bind:valid="{isDateValid}" />
-        {#if isDateValid && dateOfBirthday}
-          <span class="text-6xl font-enso"
-            ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
-        {/if}
-        {#if (isDateValid && !dateOfBirthday) || (!isDateValid && dateOfBirthday)}
-          <span class="text-6xl font-enso"
-            ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
-        {/if}
-      </div>
-    </div>
-    <div class="flex flex-col mb-5 text-sm">
-      <Label key="dapps.o-homepage.components.survey.userDataCollection.myBusinessOffers" />
-      <div class="flex">
-        <DropDown
-          selected="Select what your business offers"
-          items="{businessOffersData}"
-          id="businessOffers"
-          key="id"
-          value="name"
-          on:dropDownChange="{handleOnChange}" />
-        {#if businessOffers}
-          <span class="text-6xl font-enso"
-            ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
-        {/if}
-        {#if !businessOffers}
-          <span class="text-6xl font-enso"
-            ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
-        {/if}
-      </div>
-    </div>
-  </div>
-  {#if !isValid}
-    <div class="text-sm text-info"><Label key="dapps.o-homepage.components.survey.userDataCollection.info" /></div>
-  {/if}
-  <div class="flex flex-row justify-around w-full mt-10 mb-5 text-center">
-    <div>
-      <button
-        class="relative px-8 overflow-hidden transition-all transform btn bg-cpurple border-warning text-warning"
-        on:click="{() => handleClick('back')}">
-        {$_("dapps.o-homepage.components.survey.button.goBack")}</button>
+    <div class="mt-5 mb-10 text-center">
+      <Label key="dapps.o-homepage.components.survey.userDataCollection.subtitle" />
     </div>
     <div>
-      <button
-        class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple"
-        on:click="{() => handleClick('next')}"
-        disabled="{!isValid}">
-        {$_("dapps.o-homepage.components.survey.button.next")}</button>
+      <div class="flex flex-col mb-5 text-sm">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.useCircleAs" />
+        <div class="flex">
+          <DropDown
+            selected="Select your type of user"
+            items="{typeOfUserData}"
+            id="userType"
+            key="id"
+            value="name"
+            on:dropDownChange="{handleOnChange}" />
+          {#if typeOfUser}
+            <span class="text-6xl font-enso"
+              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+          {/if}
+          {#if !typeOfUser}
+            <span class="text-6xl font-enso"
+              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+          {/if}
+        </div>
+      </div>
+      <div class="flex flex-col mb-5 text-sm">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.gender" />
+        <div class="flex">
+          <DropDown
+            selected="Select gender"
+            items="{genderOfUserData}"
+            id="gender"
+            key="id"
+            value="name"
+            on:dropDownChange="{handleOnChange}" />
+          {#if genderOfUser}
+            <span class="text-6xl font-enso"
+              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+          {/if}
+          {#if !genderOfUser}
+            <span class="text-6xl font-enso"
+              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+          {/if}
+        </div>
+      </div>
+      <div class="flex flex-col items-start mb-2 text-sm">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.bornOn" />
+        <div class="flex items-center h-12 mb-2 date-picker">
+          <DateInput
+            bind:value="{dateOfBirthday}"
+            format="MM-dd-yyyy"
+            placeholder="Choose a date"
+            closeOnSelection="{true}"
+            bind:valid="{isDateValid}" />
+          {#if isDateValid && dateOfBirthday}
+            <span class="text-6xl font-enso"
+              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+          {/if}
+          {#if (isDateValid && !dateOfBirthday) || (!isDateValid && dateOfBirthday)}
+            <span class="text-6xl font-enso"
+              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+          {/if}
+        </div>
+      </div>
+      <div class="flex flex-col mb-5 text-sm">
+        <Label key="dapps.o-homepage.components.survey.userDataCollection.myBusinessOffers" />
+        <div class="flex">
+          <DropDown
+            selected="Select what your business offers"
+            items="{businessOffersData}"
+            id="businessOffers"
+            key="id"
+            value="name"
+            on:dropDownChange="{handleOnChange}" />
+          {#if businessOffers}
+            <span class="text-6xl font-enso"
+              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+          {/if}
+          {#if !businessOffers}
+            <span class="text-6xl font-enso"
+              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+          {/if}
+        </div>
+      </div>
+    </div>
+    {#if !isValid}
+      <div class="text-sm text-info"><Label key="dapps.o-homepage.components.survey.userDataCollection.info" /></div>
+    {/if}
+    <div class="flex flex-row justify-around w-full mt-10 mb-5 text-center">
+      <div>
+        <button
+          class="relative px-8 overflow-hidden transition-all transform btn bg-cpurple border-warning text-warning"
+          on:click="{() => handleClick('back')}">
+          {$_("dapps.o-homepage.components.survey.button.goBack")}</button>
+      </div>
+      <div>
+        <button
+          class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple"
+          on:click="{() => handleClick('next')}"
+          disabled="{!isValid}">
+          {$_("dapps.o-homepage.components.survey.button.next")}</button>
+      </div>
     </div>
   </div>
 </div>
