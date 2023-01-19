8fbfaa5d22a9e3fb709076fd79496d80b0387c49
Thorsten Schau <letsgo@codeho.net>
2023-01-19 15:03:03 +0100

    adjust dashboard card style and icon size

--- shell/src/dapps/o-dashboard/pages/Home.svelte
+++ shell/src/dapps/o-dashboard/pages/Home.svelte
@@ -62,7 +62,7 @@ onMount(init);
       <DashboardColorCard
         color="marketplace"
         link="/market/listing"
-        blobshape="175% 0% 92% 93% / 110% 32% 110% 81%"
+        blobshape="75% 0% 92% 93% / 110% 32% 110% 81%"
         icon="marketlisting"
         title="dapps.o-dashboard.pages.home.marketlisting" />
     </div>
