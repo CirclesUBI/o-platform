b95d57ce875263ae1ea0d6c46542ad3a463e0cb3
Dacian Avram~ <daciancatalin20@gmail.com~>
2023-02-13 22:47:12 +0200

    changed route for survey

--- shell/src/dapps/o-survey/components/InformedConsentSecondSurvey.svelte
+++ shell/src/dapps/o-survey/components/InformedConsentSecondSurvey.svelte
@@ -19,7 +19,7 @@ myForm.validate();
 
 function handleClick(button) {
   if (button === "back") {
-    push("#/homepage/survey/2");
+    push("#/survey/2");
   } else {
     if ($myForm.valid) {
       $surveyConsents.exchangeConsent = $exchange.value;
@@ -27,7 +27,7 @@ function handleClick(button) {
       $surveyConsents.onlyFromFriends = $onlyFromFriends.value;
       $surveyConsents.allConsentsGiven = true;
       sessionStorage.setItem("surveyConsentPage2", "true");
-      push("#/homepage/survey/4");
+      push("#/survey/4");
     } else {
       sessionStorage.setItem("surveyConsentPage2", "false");
     }
