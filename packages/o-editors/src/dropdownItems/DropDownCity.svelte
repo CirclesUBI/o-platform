454548e4ba30f0a0addd000402d2b17ae485941d
Thorsten Schau <letsgo@codeho.net>
2023-02-28 11:09:03 +0100

    location stuff

--- packages/o-editors/src/dropdownItems/DropDownCity.svelte
+++ packages/o-editors/src/dropdownItems/DropDownCity.svelte
@@ -1,18 +1,12 @@
 <script type="ts">
+import { buildAddressString } from "../../../../shell/src/shared/functions/locationHandler";
+
 export let item = undefined;
-// export let isActive = false;
+
 export let isFirst = false;
 export let isHover = false;
 export let getHighlight = undefined;
 
-function buildAddressString(address) {
-  let addr: string[] = [address.street, address.houseNumber, address.city, address.district, address.countryCode];
-  addr = addr.filter(function (element) {
-    return element !== undefined;
-  });
-
-  return addr.join(", ");
-}
 const getLabel = (value) => {
   let label = value;
   console.log(getHighlight);
@@ -24,8 +18,6 @@ const getLabel = (value) => {
     label = `${beforeHighlight}<b>${highlightContent}</b>${afterHighlight}`;
   }
   return buildAddressString(item.address);
-  console.log("ITEM", item);
-  return label;
 };
 
 let label = "";
@@ -34,11 +26,6 @@ let itemClasses = "";
 $: {
   const classes = [];
 
-  // if (isActive) {
-  //   classes.push("active border-primary");
-  // } else {
-  //   classes.push("border-light");
-  // }
   classes.push("border-light");
   if (isFirst) {
     classes.push("first");
@@ -54,13 +41,10 @@ $: {
 
 <section class="flex mb-4 mr-1 items-center justify-center  border rounded-lg shadow-sm customItem  {itemClasses}">
   <div class="flex items-center w-full p-0 space-x-2 sm:space-x-6 item-body ">
-    <div class="relative flex-grow p-3 text-left truncate">
-      <div class="max-w-full -mt-1 leading-8 cursor-pointer truncateThis">
+    <div class="relative flex-grow p-3 text-left ">
+      <div class="max-w-full -mt-1 leading-8 cursor-pointer ">
         {@html label}
       </div>
-      <!-- <div class="text-xs text-left text-dark-lightest">
-        {item.country}
-      </div> -->
     </div>
   </div>
 </section>
@@ -74,16 +58,6 @@ $: {
   overflow: hidden;
   @apply bg-white;
 }
-
-/* .customItem.active {
-  @apply border;
-  @apply border-primary;
-}
-
-.customItem.hover:not(.active) {
-  @apply border;
-  @apply border-primary;
-} */
 
 .customItem_title {
   overflow: hidden;
