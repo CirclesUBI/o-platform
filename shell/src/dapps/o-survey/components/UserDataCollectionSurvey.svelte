ce1356d545403996c6dfcc76d34ab386abf44eaa
codeho <thorsten@codeho.org>
2023-03-06 12:37:25 +0100

    simple validation if QR code is a invite link

--- shell/src/dapps/o-survey/components/UserDataCollectionSurvey.svelte
+++ shell/src/dapps/o-survey/components/UserDataCollectionSurvey.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { surveyConsents, surveyData, inviteUrl } from "../stores/surveyStore";
-import { onMount } from "svelte";
+import { onMount, onDestroy } from "svelte";
 import { SurveyDataDocument, SurveyData, SurveyDataInput } from "../../../shared/api/data/types";
 import Label from "../../../shared/atoms/Label.svelte";
 import { GenderOfUser } from "../../../shared/models/GenderOfUser.model";
@@ -13,6 +13,7 @@ import { DateInput } from "date-picker-svelte";
 import { form, field } from "svelte-forms";
 import { required } from "svelte-forms/validators";
 import { generateLongId } from "../../../shared/functions/generateRandomUid";
+import { error } from "../../../shared/stores/error";
 
 const typeOfUserData = TypeOfUser;
 const genderOfUserData = GenderOfUser;
@@ -27,6 +28,8 @@ const gender = field("gender", "", [required()]);
 const dateOfBirth = field("dateOfBirth", <Date>null, [required()]);
 const invite = field("invite", "", [required()]);
 const myForm = form(userType, gender, dateOfBirth, invite);
+
+onDestroy(() => {});
 
 myForm.validate();
 
@@ -105,76 +108,59 @@ function handleOnChange(event) {
             on:dropDownChange="{handleOnChange}" />
 
           {#if $userType.value && $userType.value !== "undefined"}
-            <span class="text-6xl font-enso"
-              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+            <span class="text-6xl font-enso"><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
           {:else}
-            <span class="text-6xl font-enso"
-              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+            <span class="text-6xl font-enso"><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
           {/if}
         </div>
       </div>
       <div class="flex flex-col mb-5 text-sm">
         <Label key="dapps.o-homepage.components.survey.userDataCollection.gender" />
         <div class="flex">
-          <DropDown
-            selected="Select gender"
-            items="{genderOfUserData}"
-            id="gender"
-            key="id"
-            value="name"
-            dropDownClass="max-w-xs text-base"
-            on:dropDownChange="{handleOnChange}" />
+          <DropDown selected="Select gender" items="{genderOfUserData}" id="gender" key="id" value="name" dropDownClass="max-w-xs text-base" on:dropDownChange="{handleOnChange}" />
           {#if $gender.value && $gender.value !== "undefined"}
-            <span class="text-6xl font-enso"
-              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+            <span class="text-6xl font-enso"><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
           {:else}
-            <span class="text-6xl font-enso"
-              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+            <span class="text-6xl font-enso"><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
           {/if}
         </div>
       </div>
       <div class="flex flex-col items-start mb-2 text-sm">
         <Label key="dapps.o-homepage.components.survey.userDataCollection.bornOn" />
         <div class="flex items-center h-12 mb-2 date-picker">
-          <DateInput
-            bind:value="{$dateOfBirth.value}"
-            min="{new Date('1920-01-01')}"
-            format="MM-dd-yyyy"
-            placeholder="Choose a date"
-            closeOnSelection="{true}" />
+          <DateInput bind:value="{$dateOfBirth.value}" min="{new Date('1920-01-01')}" format="MM-dd-yyyy" placeholder="Choose a date" closeOnSelection="{true}" />
 
           {#if $dateOfBirth.value}
-            <span class="text-6xl font-enso"
-              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+            <span class="text-6xl font-enso"><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
           {:else}
-            <span class="text-6xl font-enso"
-              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+            <span class="text-6xl font-enso"><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
           {/if}
         </div>
       </div>
 
       <div class="flex flex-col items-start w-full mb-2 text-sm">
         <Label key="dapps.o-homepage.components.survey.userDataCollection.scanInvite" />
+
         <div class="flex items-center w-full h-12 pr-8 mb-2">
-          <button
-            class="px-8 overflow-hidden transition-all transform btn btn-primary btn-block"
-            on:click="{() => handleClick('openQRCode')}"
-            disabled="{$inviteUrl}">
+          <button class="px-8 overflow-hidden transition-all transform btn btn-primary btn-block" on:click="{() => handleClick('openQRCode')}" disabled="{$inviteUrl}">
             <!-- {$_("dapps.o-homepage.components.survey.button.scanInviteNow")} -->
             Scan Invite Now
           </button>
+
           <input type="hidden" bind:value="{$invite.value}" />
 
           {#if $inviteUrl}
-            <span class="text-6xl font-enso"
-              ><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
+            <span class="text-6xl font-enso"><Icons icon="check-circle" size="{6}" customClass="inline ml-2 text-success" /></span>
           {:else}
-            <span class="text-6xl font-enso"
-              ><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
+            <span class="text-6xl font-enso"><Icons icon="information-circle" size="{6}" customClass="inline ml-2 text-alert" /></span>
           {/if}
         </div>
       </div>
     </div>
+    {#if $error}
+      <p class="mb-2 text-sm text-center text-alert ">{$error}</p>
+    {/if}
+
     {#if !$myForm.valid}
       <div class="text-sm text-center text-info">
         <Label key="dapps.o-homepage.components.survey.userDataCollection.info" />
@@ -182,17 +168,12 @@ function handleOnChange(event) {
     {/if}
     <div class="flex flex-row justify-around w-full mt-10 mb-5 text-center">
       <div>
-        <button
-          class="relative px-8 overflow-hidden transition-all transform btn bg-cpurple border-warning text-warning"
-          on:click="{() => handleClick('back')}">
+        <button class="relative px-8 overflow-hidden transition-all transform btn bg-cpurple border-warning text-warning" on:click="{() => handleClick('back')}">
           {$_("dapps.o-homepage.components.survey.button.goBack")}</button>
       </div>
       <div>
         {#if $myForm.dirty}
-          <button
-            class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple"
-            on:click="{() => handleClick('next')}"
-            disabled="{!$myForm.valid}">
+          <button class="relative px-16 overflow-hidden transition-all transform btn btn-primary bg-primary text-cpurple" on:click="{() => handleClick('next')}" disabled="{!$myForm.valid}">
             {$_("dapps.o-homepage.components.survey.button.next")}</button>
         {/if}
       </div>
