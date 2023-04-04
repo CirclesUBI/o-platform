79c5ff5f723865e934c629b0b78544ee72e2d821
jaensen <4954577+jaensen@users.noreply.github.com>
2023-04-04 21:44:05 +0200

    added a $unreadEventCount svelte store and used it to indicate how many unread notifications there are in the NextNav's NavPill. Include the 'unread_marker_id' in the event response

--- shell/src/dapps/o-notifications/pages/Home.svelte
+++ shell/src/dapps/o-notifications/pages/Home.svelte
@@ -3,19 +3,22 @@ import SimpleHeader from "src/shared/atoms/SimpleHeader.svelte";
 import { RuntimeDapp } from "@o-platform/o-interfaces/dist/runtimeDapp";
 import { Routable } from "@o-platform/o-interfaces/dist/routable";
 import { EventType, SortOrder } from "../../../shared/api/data/types";
-import { _ } from "svelte-i18n";
-import Label from "../../../shared/atoms/Label.svelte";
 import { MyInbox } from "../../../shared/stores/inbox";
 import EventList from "../../../shared/molecules/Lists/EventList.svelte";
 import TransactionCard from "../../o-banking/atoms/TransactionCard.svelte";
 import RedeemedInvitationCard from "../atoms/RedeemedInvitationCard.svelte";
-import GenericEventCard from "../atoms/GenericEventCard.svelte";
 import ContactCard from "../../o-notifications/atoms/ContactCard.svelte";
 
 export let runtimeDapp: RuntimeDapp<any>;
 export let routable: Routable;
 
-let inbox = new MyInbox(SortOrder.Desc, 30, [EventType.CrcHubTransfer, EventType.CrcMinting, EventType.CrcTrust, EventType.InvitationRedeemed, EventType.Erc20Transfer]);
+let inbox = new MyInbox(
+        SortOrder.Desc,
+        30,
+        [EventType.CrcHubTransfer, EventType.CrcMinting, EventType.CrcTrust, EventType.InvitationRedeemed, EventType.Erc20Transfer],
+        {
+          unreadOnly: true
+        });
 
 const views = {
   [EventType.CrcHubTransfer]: { component: TransactionCard },
--- shell/src/shared/molecules/NextNav/Components/Notification.svelte
+++ shell/src/shared/molecules/NextNav/Components/Notification.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import Icons from "./../../Icons.svelte";
-  import { inbox } from "../../../stores/inbox";
+  import {unreadEventCount} from "../../../stores/unreadEventCount";
 </script>
 
 <div class="relative text-secondary">
   <Icons icon="notificationbubble" />
   <div class="absolute top-0 w-full text-center text-white font-heading">
-    {$inbox.length}asd
+    {$unreadEventCount}
   </div>
 </div>
