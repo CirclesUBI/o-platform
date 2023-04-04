c1df455490898b5c348100ebae7935a3df01c878
Thorsten Schau <thorsten@codeho.org>
2023-04-04 12:02:17 +0200

    dont allow orgas to be searched for trust

--- shell/src/dapps/o-banking/processes/setTrust.ts
+++ shell/src/dapps/o-banking/processes/setTrust.ts
@@ -1,37 +1,35 @@
-import {ProcessDefinition} from "@o-platform/o-process/dist/interfaces/processManifest";
-import {ProcessContext} from "@o-platform/o-process/dist/interfaces/processContext";
-import {fatalError} from "@o-platform/o-process/dist/states/fatalError";
-import {createMachine} from "xstate";
-import {prompt} from "@o-platform/o-process/dist/states/prompt";
-import {PlatformEvent} from "@o-platform/o-events/dist/platformEvent";
-import {GnosisSafeProxy} from "@o-platform/o-circles/dist/safe/gnosisSafeProxy";
-import {RpcGateway} from "@o-platform/o-circles/dist/rpcGateway";
-import {CirclesHub} from "@o-platform/o-circles/dist/circles/circlesHub";
-import {BN} from "ethereumjs-util";
-import {EditorViewContext} from "@o-platform/o-editors/src/shared/editorViewContext";
+import { ProcessDefinition } from "@o-platform/o-process/dist/interfaces/processManifest";
+import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
+import { fatalError } from "@o-platform/o-process/dist/states/fatalError";
+import { createMachine } from "xstate";
+import { prompt } from "@o-platform/o-process/dist/states/prompt";
+import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
+import { GnosisSafeProxy } from "@o-platform/o-circles/dist/safe/gnosisSafeProxy";
+import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
+import { CirclesHub } from "@o-platform/o-circles/dist/circles/circlesHub";
+import { BN } from "ethereumjs-util";
+import { EditorViewContext } from "@o-platform/o-editors/src/shared/editorViewContext";
 import HtmlViewer from "@o-platform/o-editors/src/HtmlViewer.svelte";
 import TrustChangeConfirmation from "../molecules/TrustChangeConfirmation.svelte";
-import {promptCirclesSafe} from "../../../shared/api/promptCirclesSafe";
-import type {TransactionReceipt} from "web3-core";
-import {Environment} from "../../../shared/environment";
-
-import {
-  Contact,
-  ContactDirection,
-  Profile,
-  ProfilesByCirclesAddressDocument,
-  ProfilesByCirclesAddressQueryVariables,
-} from "../../../shared/api/data/types";
-import {ApiClient} from "../../../shared/apiConnection";
-import {loadProfile} from "../../o-passport/processes/identify/services/loadProfile";
-import {me} from "../../../shared/stores/me";
-import {contacts} from "../../../shared/stores/contacts";
+import { promptCirclesSafe } from "../../../shared/api/promptCirclesSafe";
+import type { TransactionReceipt } from "web3-core";
+import { Environment } from "../../../shared/environment";
+import { setWindowLastError } from "../../../shared/processes/actions/setWindowLastError";
+import { Contact, ContactDirection, Profile, ProfileType, ProfilesByCirclesAddressDocument, ProfilesByCirclesAddressQueryVariables } from "../../../shared/api/data/types";
+import { ApiClient } from "../../../shared/apiConnection";
+import { loadProfile } from "../../o-passport/processes/identify/services/loadProfile";
+import { me } from "../../../shared/stores/me";
+import { contacts } from "../../../shared/stores/contacts";
+import { throwError } from "rxjs";
+import { show } from "@o-platform/o-process/dist/actions/show";
+import ErrorView from "../../../shared/atoms/Error.svelte";
 
 export type SetTrustContextData = {
   safeAddress: string;
   hubAddress: string;
   privateKey: string;
   profile?: Profile;
+  profileType?: ProfileType;
   trustReceiver?: string;
   trustLimit?: number;
   successAction?: (data: SetTrustContextData) => void;
@@ -81,41 +79,32 @@ const editorContent: { [x: string]: EditorViewContext } = {
  * Checks which users are trusted by the current user and automatically trusts these users also in all orgas.
  * @param mainSafeAddress
  */
-async function hSetTrust(mainSafeAddress:string) {
-    let $contacts:Contact[];
-    contacts.subscribe(value => $contacts = value)();
+async function hSetTrust(mainSafeAddress: string) {
+  let $contacts: Contact[];
+  contacts.subscribe((value) => ($contacts = value))();
 
-    const iTrust = $contacts.filter(contact => {
-      const trustMetadata = contact.metadata.find(metadata => metadata.name == "CrcTrust");
-      if (!trustMetadata) return false;
+  const iTrust = $contacts.filter((contact) => {
+    const trustMetadata = contact.metadata.find((metadata) => metadata.name == "CrcTrust");
+    if (!trustMetadata) return false;
 
-      const outDirectionIdx = trustMetadata.directions.indexOf(ContactDirection.Out);
-      if (outDirectionIdx == -1) return false;
+    const outDirectionIdx = trustMetadata.directions.indexOf(ContactDirection.Out);
+    if (outDirectionIdx == -1) return false;
 
-      const outTrustLimit = trustMetadata.values[outDirectionIdx];
-      return {
-        contactAddress: contact.contactAddress,
-        trustLimit: outTrustLimit
-      };
-    });
+    const outTrustLimit = trustMetadata.values[outDirectionIdx];
+    return {
+      contactAddress: contact.contactAddress,
+      trustLimit: outTrustLimit,
+    };
+  });
 
-    let $me:Profile;
-    me.subscribe(value => $me = value)();
-    const myAdminMemberships =  $me.memberships
-          ?.filter(o => o.isAdmin)
-            ?.map(o => o.organisation.circlesAddress)
-            ?? [];
-
-
-
+  let $me: Profile;
+  me.subscribe((value) => ($me = value))();
+  const myAdminMemberships = $me.memberships?.filter((o) => o.isAdmin)?.map((o) => o.organisation.circlesAddress) ?? [];
 }
 
-async function gSetTrust(mainSafeAddress:string, trustReceiver:string, limit: number) {
+async function gSetTrust(mainSafeAddress: string, trustReceiver: string, limit: number) {
   const myProfile = await loadProfile(mainSafeAddress);
-  const myAdminMemberships =  myProfile.memberships
-      ?.filter(o => o.isAdmin)
-      ?.map(o => o.organisation.circlesAddress)
-      ?? [];
+  const myAdminMemberships = myProfile.memberships?.filter((o) => o.isAdmin)?.map((o) => o.organisation.circlesAddress) ?? [];
 
   const web3 = await RpcGateway.get();
   const safeProxy = new GnosisSafeProxy(web3, myProfile.circlesAddress);
@@ -131,7 +120,7 @@ async function gSetTrust(mainSafeAddress:string, trustReceiver:string, limit: nu
     },
     dirtyFlags: {},
     messages: {},
-    onlyThesePages: undefined
+    onlyThesePages: undefined,
   });
   console.log("personTrustReceipt.transactionHash: ", personTrustReceipt.transactionHash);
 
@@ -147,7 +136,7 @@ async function gSetTrust(mainSafeAddress:string, trustReceiver:string, limit: nu
         },
         dirtyFlags: {},
         messages: {},
-        onlyThesePages: undefined
+        onlyThesePages: undefined,
       });
       console.log("followTrust.transactionHash: ", followTrust.transactionHash);
     } catch (e) {
@@ -188,18 +177,13 @@ const processDefinition = (processId: string) =>
           },
         ],
       },
+
       trustReceiver: promptCirclesSafe<SetTrustContext, any>({
         field: "trustReceiver",
         onlyWhenDirty: false,
         params: {
-          view: (editorContent.recipient = {
-            title: window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.recipient.title"),
-            description: "",
-            placeholder: window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.recipient.placeholder"),
-            submitButtonText: window.o.i18n(
-              "dapps.o-banking.processes.setTrust.editorContent.recipient.submitButtonText"
-            ),
-          }),
+          profileType: ProfileType.Person,
+          view: editorContent.recipient,
           placeholder: editorContent.recipient.placeholder,
           submitButtonText: editorContent.recipient.submitButtonText,
         },
@@ -207,20 +191,75 @@ const processDefinition = (processId: string) =>
           next: "#checkTrustLimit",
         },
       }),
+
       checkTrustLimit: {
         id: "checkTrustLimit",
         always: [
           {
             cond: (context) => context.data.trustReceiver.toLowerCase() == context.data.safeAddress.toLowerCase(),
             actions: (context) => {
-              context.messages["trustReceiver"] = window.o.i18n(
-                "dapps.o-banking.processes.setTrust.checkTrustLimit.contextMessage"
-              );
+              console.log("TRUSTRECEIVER", context.data.trustReceiver);
+              editorContent.recipient.description = "HERRGOTSACK";
             },
             target: "#trustReceiver",
           },
           {
             cond: (context) => context.data.trustLimit !== undefined && context.data.trustLimit !== null,
+            target: "#getProfile",
+          },
+          {
+            target: "#trustReceiver",
+          },
+        ],
+      },
+
+      getProfile: {
+        id: "getProfile",
+        entry: () => {
+          window.o.publishEvent(<PlatformEvent>{
+            type: "shell.progress",
+            message: window.o.i18n("dapps.o-banking.processes.setTrust.setTrust.gettingProfile"),
+          });
+        },
+        invoke: {
+          src: async (context) => {
+            try {
+              const result = await ApiClient.query<Profile, ProfilesByCirclesAddressQueryVariables>(ProfilesByCirclesAddressDocument, {
+                circlesAddresses: [context.data.trustReceiver],
+              });
+              context.data.profile = result;
+            } catch (error) {
+              console.info(`Could not load Profile for circlesAddress: ${context.data.trustReceiver}`, error);
+            }
+            return context.data.profile;
+          },
+          onDone: "#verifyProfile",
+
+          onError: {
+            target: "#showError",
+            actions: setWindowLastError,
+          },
+        },
+      },
+
+      verifyProfile: {
+        id: "verifyProfile",
+        entry: () => {
+          window.o.publishEvent(<PlatformEvent>{
+            type: "shell.progress",
+            message: window.o.i18n("dapps.o-banking.processes.setTrust.setTrust.verifyingProfile"),
+          });
+        },
+        always: [
+          {
+            cond: (context) => context.data.profile[0].type == "ORGANISATION",
+            actions: (context) => {
+              context.messages["trustReceiver"] = window.o.i18n("dapps.o-banking.processes.setTrust.notAPersonalProfile");
+            },
+            target: "#trustReceiver",
+          },
+          {
+            cond: (context) => context.data.profile[0].type !== "ORGANISATION",
             target: "#setTrust",
           },
           {
@@ -229,8 +268,6 @@ const processDefinition = (processId: string) =>
         ],
       },
 
-      // The code was either manually entered or pre-configured at launch.
-      // Exchange it for the actual token and redirect the user to the application.
       setTrust: {
         id: "setTrust",
         entry: () => {
@@ -242,24 +279,16 @@ const processDefinition = (processId: string) =>
         invoke: {
           src: async (context) => {
             try {
-              const result = await ApiClient.query<Profile, ProfilesByCirclesAddressQueryVariables>(
-                ProfilesByCirclesAddressDocument,
-                {
-                  circlesAddresses: [context.data.trustReceiver],
-                }
-              );
-              context.data.profile = result;
+              await gSetTrust(context.data.safeAddress, context.data.trustReceiver, context.data.trustLimit);
             } catch (error) {
-              console.info(`Could not load Profile for circlesAddress: ${context.data.trustReceiver}`);
+              console.info(`${window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.error.trustfailed")} ${context.data.trustReceiver}`, error);
             }
-
-            return await gSetTrust(context.data.safeAddress, context.data.trustReceiver, context.data.trustLimit);
           },
           onDone: "#showSuccess",
 
           onError: {
-            target: "error",
-            actions: console.log,
+            target: "#showError",
+            actions: setWindowLastError,
           },
         },
       },
@@ -273,9 +302,7 @@ const processDefinition = (processId: string) =>
             view: (editorContent.success = {
               title: window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.success.title"),
               description: "",
-              submitButtonText: window.o.i18n(
-                "dapps.o-banking.processes.setTrust.editorContent.success.submitButtonText"
-              ),
+              submitButtonText: window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.success.submitButtonText"),
             }),
             profile: context.data.profile,
             trustLimit: context.data.trustLimit,
@@ -288,22 +315,19 @@ const processDefinition = (processId: string) =>
         },
       }),
 
-      error: prompt({
-        id: "error",
-        field: "__",
-        component: HtmlViewer,
-        params: {
-          view: (editorContent.message = {
-            title: window.o.i18n("dapps.o-banking.processes.setTrust.editorContent.message.title"),
-            description: "",
-            submitButtonText: window.o.i18n(
-              "dapps.o-banking.processes.setTrust.editorContent.message.submitButtonText"
-            ),
-          }),
-          html: () => "Oopsie, Something went wrong, please close this window and try again. ",
-          hideNav: false,
-        },
-      }),
+      showError: {
+        id: "showError",
+        entry: show({
+          component: ErrorView,
+          params: {},
+          field: {
+            name: "",
+            get: () => undefined,
+            set: (o: any) => {},
+          },
+        }),
+      },
+
       success: {
         type: "final",
         id: "success",
