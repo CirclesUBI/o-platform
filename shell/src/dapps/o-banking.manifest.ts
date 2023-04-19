037457ca09791ba2865ddfb2475aa101b63d3874
Thorsten Schau <thorsten@codeho.org>
2023-04-19 17:02:19 +0200

    change sendCircles icon everywhere i could find

--- shell/src/dapps/o-banking.manifest.ts
+++ shell/src/dapps/o-banking.manifest.ts
@@ -135,7 +135,7 @@ const transferTrigger: Trigger<any, BankingDappState> = {
     });
   },
   title: "common.sendMoney",
-  icon: "cash",
+  icon: "sendmoney",
   type: "trigger",
 };
 
@@ -158,7 +158,7 @@ const transferTriggerRedirect: Trigger<any, BankingDappState> = {
     });
   },
   title: "common.sendMoney",
-  icon: "cash",
+  icon: "sendmoney",
   type: "trigger",
 };
 
