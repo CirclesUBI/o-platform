5d6ee81d30f36052c41222bdfb9ffda5d6ab63d1
Thorsten Schau <thorsten@codeho.org>
2023-04-20 13:31:01 +0200

    restyled common error component, added component to some editors - needs overhaul

--- packages/o-editors/src/NumberEditor.svelte
+++ packages/o-editors/src/NumberEditor.svelte
@@ -3,7 +3,7 @@ import { EditorContext } from "./editorContext";
 import ProcessNavigation from "./ProcessNavigation.svelte";
 import { Continue } from "@o-platform/o-process/dist/events/continue";
 import { onMount } from "svelte";
-
+import Error from "../../../shell/src/shared/atoms/Error.svelte";
 export let context: EditorContext;
 
 let _context: EditorContext;
@@ -13,9 +13,7 @@ $: {
 
 console.log("INNER CONTEXT : ", context);
 let inputField: any;
-let fieldId = context.isSensitive
-  ? Math.random().toString().replace(".", "")
-  : context.field;
+let fieldId = context.isSensitive ? Math.random().toString().replace(".", "") : context.field;
 
 const submitHandler = () => {
   const answer = new Continue();
@@ -32,23 +30,7 @@ function onkeydown(e: KeyboardEvent) {
 
 <div class="form-control justify-self-center">
   {#if context.messages[context.field]}
-    <div class="mt-2 mb-2 alert alert-error">
-      <div class="flex-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="w-6 h-6 mx-2 stroke-current">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-          ></path>
-        </svg>
-        <label for="input">{context.messages[context.field]} </label>
-      </div>
-    </div>
+    <Error message="{context.messages[context.field]}" />
   {/if}
   <input
     on:keydown="{onkeydown}"
