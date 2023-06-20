db2459096f70660431e60ae2ac8f1b8de6ebb907
Thorsten Schau <letsgo@codeho.net>
2023-06-20 13:52:57 +0200

    Auto stash before rebase of "origin/dev"

--- shell/src/dapps/o-onboarding/processes/initContext.ts
+++ shell/src/dapps/o-onboarding/processes/initContext.ts
@@ -1,15 +1,9 @@
-import {
-  EoaData,
-  ProfileData,
-  RegistrationData,
-  SafeData,
-  UbiData
-} from "./initEvent";
-import {ClaimedInvitation, Profile, ProfileEvent, SafeInfo, SessionInfo} from "../../../shared/api/data/types";
-import {OpenloginUserInfo} from "@toruslabs/openlogin";
+import { EoaData, ProfileData, RegistrationData, SafeData, UbiData } from "./initEvent";
+import { ClaimedInvitation, Profile, ProfileEvent, SafeInfo, SessionInfo } from "../../../shared/api/data/types";
+import type { OpenloginUserInfo } from "../../../shared/openLogin";
 
 export type InitContext = {
-  localStorageSchemaVersion: 1,
+  localStorageSchemaVersion: 1;
   useMockProfileIndex?: number;
   session?: SessionInfo;
   openLoginUserInfo: OpenloginUserInfo;
@@ -19,5 +13,5 @@ export type InitContext = {
   eoa?: EoaData;
   eoaInvitationTransaction?: ProfileEvent;
   safe?: SafeData;
-  ubi?: UbiData,
+  ubi?: UbiData;
 };
