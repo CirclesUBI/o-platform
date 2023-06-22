63caba8fac2a29bf6a0cc80d44c59c03648084d1
Thorsten Schau <thorsten@codeho.org>
2023-06-22 15:07:18 +0200

    preliminary commit openLogin 4.x

--- shell/src/shared/openLogin.ts
+++ shell/src/shared/openLogin.ts
@@ -1,11 +1,10 @@
-import { getED25519Key } from "@toruslabs/openlogin-ed25519";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-import * as bs58 from "bs58";
-import OpenLogin from "@toruslabs/openlogin";
+
+import OpenLogin from "openlogin";
 import { LoginParams, LOGIN_PROVIDER, LOGIN_PROVIDER_TYPE, UX_MODE, UX_MODE_TYPE, OPENLOGIN_NETWORK, OPENLOGIN_NETWORK_TYPE } from "@toruslabs/openlogin-utils";
 import loginConfig from "./loginConfig";
 import { Environment } from "./environment";
-
+import whitelabel from "./torusWhiteLabel";
 export type OpenloginUserInfo = {
   email?: string;
   name?: string;
@@ -21,16 +20,8 @@ let privKey: string = "";
 let ethereumPrivateKeyProvider: EthereumPrivateKeyProvider | null = null;
 let selectedLoginProvider: LOGIN_PROVIDER_TYPE = LOGIN_PROVIDER.GOOGLE;
 let login_hint: string = "";
-let isWhiteLabelEnabled: boolean = false;
-let selectedUxMode: UX_MODE_TYPE = UX_MODE.REDIRECT;
-let selectedOpenloginNetwork: OPENLOGIN_NETWORK_TYPE = OPENLOGIN_NETWORK.TESTNET;
-
-// const openlogin = this.openloginInstance;
-// await openlogin.init();
-// if (openlogin.privKey) {
-//   this.privKey = openlogin.privKey;
-//   await this.setProvider(this.privKey);
-// }
+let selectedUxMode: UX_MODE_TYPE = UX_MODE.POPUP;
+let selectedOpenloginNetwork: OPENLOGIN_NETWORK_TYPE = OPENLOGIN_NETWORK.MAINNET;
 
 export async function getOpenLogin(): Promise<OpenLogin> {
   const currentClientId = Environment.openLoginClientId;
@@ -38,8 +29,9 @@ export async function getOpenLogin(): Promise<OpenLogin> {
     clientId: currentClientId,
     network: selectedOpenloginNetwork,
     uxMode: selectedUxMode,
-    whiteLabel: {},
-    // loginConfig: loginConfig,
+    loginConfig: loginConfig,
+    whiteLabel: whitelabel,
+
     // sdkUrl: "https://staging.openlogin.com",
   });
   op.init();
