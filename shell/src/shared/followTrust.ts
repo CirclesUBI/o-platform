2b2d964652bc6ec2751cc829418b73f505142f3e
Ingo Collatz <incollatz@mail.de>
2023-06-22 15:49:44 +0200

    add logout-function to openLogin, dont't followtrust without session

--- shell/src/shared/followTrust.ts
+++ shell/src/shared/followTrust.ts
@@ -3,6 +3,9 @@ import { me } from "./stores/me";
 import { ApiClient } from "./apiConnection";
 import { fSetTrust } from "../dapps/o-banking/processes/setTrust";
 import { Environment } from "./environment";
+import { get } from 'svelte/store'
+
+const $me = get(me)
 
 export class FollowTrust {
   static readonly shortIntervalInMilliseconds = 6000;
@@ -12,7 +15,7 @@ export class FollowTrust {
   private intervalHandle: any;
   private isWorking = false;
 
-  constructor() {}
+  constructor() { }
 
   reset() {
     this.stop();
@@ -25,7 +28,9 @@ export class FollowTrust {
 
     this.intervalHandle = setInterval(() => {
       // console.log(`FollowTrust is running. (isWorking: ${this.isWorking})`);
-      if (this.isWorking) {
+      const $me = get(me)
+
+      if (this.isWorking || !$me?.id) {
         return;
       }
       this.followTrust()
