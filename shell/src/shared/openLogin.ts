44349892607cc825e9499a51d5323e9fb62965ff
Ingo Collatz <incollatz@mail.de>
2023-06-22 15:49:44 +0200

    add logout-function to openLogin, dont't followtrust without session

--- shell/src/shared/openLogin.ts
+++ shell/src/shared/openLogin.ts
@@ -1,14 +1,13 @@
-import OpenLogin, {OpenloginUserInfo} from "@toruslabs/openlogin";
-import {RpcGateway} from "@o-platform/o-circles/dist/rpcGateway";
-import {Environment} from "./environment";
+import OpenLogin, { OpenloginUserInfo } from "@toruslabs/openlogin";
+import { RpcGateway } from "@o-platform/o-circles/dist/rpcGateway";
+import { Environment } from "./environment";
 
 let openLogin: OpenLogin;
 
-export type GetOpenLoginResult = OpenLogin|{login(args:any):{privKey:string}, getUserInfo():{userInfo:any}, logout():Promise<void>};
+export type GetOpenLoginResult = OpenLogin | { login(args: any): { privKey: string }, logout(): Promise<void>, getUserInfo(): { userInfo: any }, logout(): Promise<void> };
 
 export async function getOpenLogin(): Promise<GetOpenLoginResult> {
-  if (Environment.useMockLogin)
-  {
+  if (Environment.useMockLogin) {
     return <GetOpenLoginResult>{
       async login(params: any): Promise<{ privKey: string }> {
         const acc = RpcGateway.get().eth.accounts.create();
@@ -16,6 +15,7 @@ export async function getOpenLogin(): Promise<GetOpenLoginResult> {
           privKey: acc.privateKey
         };
       },
+      async logout(): Promise<void> { },
       async getUserInfo(): Promise<OpenloginUserInfo> {
         return {
           email: "email@some.hostname.of.some.mailserver.somewhere",
