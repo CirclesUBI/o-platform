4f95ab0b8f952bd9440b92f5bfc9bdc6cfead7c8
jaensen <4954577+jaensen@users.noreply.github.com>
2023-06-22 15:16:14 +0200

    don't execute next() if there is no $me

--- shell/src/shared/stores/pagedEventQuery.ts
+++ shell/src/shared/stores/pagedEventQuery.ts
@@ -1,14 +1,4 @@
-import {
-  EventPayload,
-  EventType,
-  PaginationArgs,
-  Profile,
-  ProfileEvent,
-  ProfileEventFilter,
-  QueryEventsArgs,
-  SortOrder,
-  StreamDocument,
-} from "../api/data/types";
+import { EventPayload, EventType, PaginationArgs, Profile, ProfileEvent, ProfileEventFilter, QueryEventsArgs, SortOrder, StreamDocument } from "../api/data/types";
 import { writable } from "svelte/store";
 import { me } from "./me";
 import { Environment } from "../environment";
@@ -114,13 +104,9 @@ export abstract class PagedEventQuery implements ObjectCache<ProfileEvent> {
   async next(): Promise<boolean> {
     let $me: Profile;
     me.subscribe((m) => ($me = m))();
-
-    // Possible Solution for avoiding thrown an Error here when the user is not logged in
-    // const sessionInfo = await me.getSessionInfo(true);
-    // if (!sessionInfo.isLoggedOn) {
-    //   console.log("NO SESSION, BAILING");
-    //   return false;
-    // }
+    if (!$me) {
+      return;
+    }
 
     const args = <QueryEventsArgs>{
       safeAddress: $me.circlesAddress,
