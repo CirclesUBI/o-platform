2a617a72109097a53b79abf660d58589b30c6e70
jaensen <4954577+jaensen@users.noreply.github.com>
2023-07-14 14:28:38 +0200

    override App.svelte with a maintenance page

--- shell/src/App.svelte
+++ shell/src/App.svelte
@@ -1,83 +1,33 @@
-<script context="module" lang="ts">
-  import {FollowTrust} from "./shared/followTrust";
-
-  const followTrust: FollowTrust = new FollowTrust();
-  followTrust.start();
-
-  export const FollowTrustWorker = followTrust;followTrust
-</script>
-<script lang="ts">
-import "./shared/css/tailwind.css";
-
-import Router from "svelte-spa-router";
-
-import DappFrame from "src/shared/molecules/DappFrame.svelte";
-import NotFound from "src/shared/pages/NotFound.svelte";
-import { interpret } from "xstate";
-import { initMachine } from "./dapps/o-onboarding/processes/init";
-import { ubiMachine } from "./shared/ubiTimer2";
-import { InitContext } from "./dapps/o-onboarding/processes/initContext";
-import { LogoutDocument } from "./shared/api/data/types";
-import { me } from "./shared/stores/me";
-import { setupI18n, isLocaleLoaded, locale } from "./i18n/i18nDictionary";
-
-import { Environment } from "./shared/environment";
-import LocaleSwitcher from "./i18n/atoms/LocaleSwitcher.svelte";
-import { onMount } from "svelte";
-
-let isLoading: boolean = true;
-
-onMount(async () => {
-  const foo = await setupI18n({ withLocale: Environment.userLanguage.slice(0, 2) });
-  isLoading = false;
-});
-
-let ubiMachineInterpreter: any;
-const v = 1;
-const currentLocalStorageSchemaVersion = localStorage.getItem("localStorageSchemaVersion");
-if (!currentLocalStorageSchemaVersion || parseInt(currentLocalStorageSchemaVersion) < v) {
-  localStorage.clear();
-  sessionStorage.clear();
-  localStorage.setItem("localStorageSchemaVersion", v.toString());
-
-  window.o.apiClient.client.subscribeToResult().then((apiClient) => {
-    apiClient.mutate({
-      mutation: LogoutDocument,
-    });
-  });
-}
-
-window.runInitMachine = (context?: InitContext) => {
-  if (context) {
-    interpret(initMachine.withContext(context)).start();
-  } else {
-    interpret(initMachine).start();
-  }
-};
-let _routes = {
-  "/:dappId?/:1?/:2?/:3?/:4?/:5?/:6?": DappFrame,
-  "*": NotFound,
-};
-</script>
-
-{#if !isLoading}
-  <Router
-    routes="{_routes}"
-    on:routeLoaded="{(e) => {
-      window.o.posthog?.capture('routeLoaded', {
-        route: e.detail.route
-      });
-      if (!ubiMachineInterpreter && $me && $me.circlesAddress) {
-        ubiMachineInterpreter = interpret(ubiMachine)
-          .onEvent((event) => {
-            console.log('UBI machine event:', event);
-          })
-          .onTransition((state) => {
-            console.log('UBI machine transition:', state.value);
-          })
-          .start();
-      }
-    }}" />
-{:else}
-  <p>Loading...</p>
-{/if}
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Website Under Maintenance</title>
+  <style>
+    body {
+      background-color: #f4f4f4;
+      font-family: Arial, sans-serif;
+    }
+    .container {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+    }
+    h1 {
+      color: #333;
+      font-size: 2em;
+    }
+    p {
+      color: #666;
+    }
+  </style>
+</head>
+<body>
+<div class="container">
+  <h1>Our Website is currently under maintenance</h1>
+  <p>We apologize for the inconvenience. Please check back later.</p>
+</div>
+</body>
+</html>
